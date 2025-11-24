69ae56becf73069ea1f1788e5b354523a5c1e7ad
littensy <56808540+littensy@users.noreply.github.com>
2025-11-24 00:48:06 -0800

    test: add tests and actions

--- bin/check.sh
+++ bin/check.sh
@@ -4,8 +4,13 @@ curl -o roblox.d.luau https://luau-lsp.pages.dev/globalTypes.None.d.luau
 
 rojo sourcemap --output sourcemap.json
 
+echo "Running luau-lsp analyze"
 luau-lsp analyze --sourcemap="sourcemap.json" --ignore="**/submodules/**" --base-luaurc=".luaurc" --definitions="roblox.d.luau" src
+
+echo "Running selene"
 selene src
+
+echo "Running stylua"
 stylua --check src
 
 rm roblox.d.luau
